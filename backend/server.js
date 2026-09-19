949a32f1e87af62d020b46afce8e9c49874db680
sh4rma <shritik406@gmail.com>
2026-09-19 19:48:12 +0530

    Fix Express backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://evstore.netlify.app",
+      "https://appdevstore.netlify.app",
     ],
     methods: [
       "GET",
